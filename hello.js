f5d1c79d839b804bee00b02d9eccf74f77b3f5fa
Samantha Long <sam43092@ymail.com>
2018-09-10 13:18:24 +0000

    Updated

--- hello.js
+++ hello.js
@@ -4,15 +4,16 @@ http.createServer(function(request, response){
     response.writeHead(200);
     let html = "<html>"
         +"<head>"
+            +"<meta charset='utf-8'>"
+           +"<title>My Dog</title>"
         +"</head>"
-        +"<body bgcolor='#D3D3D3'>"
-        +"<title>My Dog</title>"
-        +"<p> Hello, this is your dog, Max.</p>"
+        +"<body bgcolor='#A2539F'>"
+        +"<p style='font-size: 28px' align='center'> Hello, this is your dog.</p><hr><br></br>"
         +"</br></br>"
-        +"<p><b>Q:</b> Why are dogs like phones?</br>"
-        +" <b>A:</b> Because they have collar IDs!</p>"
-        +"<p><b>Q:</b> What do you get when you cross a sheepdog with a rose?</br>"
-        +"<b>A:</b> A collie-flower!"
+        +"<p><b>Q:</b> Why are dogs like phones?<br></br>"
+        +"<b>A:</b><i> Because they have collar IDs!</i></p>"
+        +"<p><b>Q:</b> What do you get when you cross a sheepdog with a rose?<br></br>"
+        +"<b>A:</b><i> A collie-flower!</i></p>"
         +"</body>"
         +"</html>";
     response.write(html);
